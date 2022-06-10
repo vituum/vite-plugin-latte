d8ae46f1d009234c4f3b40abd48c351e03e143bb
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2022-06-10 21:33:49 +0200

    fix: eslint

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-import child_process from 'child_process'
+import * as childProcess from 'child_process'
 import * as fs from 'fs'
-const start = new Date();
+const start = new Date()
 
 const execSync = (cmd) => {
-  try {
-    return {
-      output: child_process.execSync(cmd).toString()
+    try {
+        return {
+            output: childProcess.execSync(cmd).toString()
+        }
+    } catch ({ output }) {
+        return {
+            error: true,
+            output: output[1].toString()
+        }
     }
-  } catch({ output }) {
-    return {
-      error: true,
-      output: output[1].toString()
-    }
-  }
 }
 
-const latte = execSync(`php index.php`)
+const latte = execSync('php index.php')
 
 if (latte.error) {
-  console.error(latte.output)
-  process.exit(1)
+    console.error(latte.output)
+    process.exit(1)
 }
-
 
 fs.writeFileSync('index.html', latte.output)
 
-setTimeout(() => console.info("\x1b[34m", `processed in`, "\x1b[33m", `${new Date() - start}ms`, "\x1b[0m"), 1);
+console.info('\x1b[34m', 'index.html', '\x1b[33m', `${new Date() - start}ms`, '\x1b[0m')
