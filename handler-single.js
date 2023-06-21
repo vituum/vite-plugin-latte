eaec48a2242386061936097bdbd8091a38ae4743
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2023-06-21 13:00:03 +0200

    fix: handler

--- handler-single.js
+++ handler-single.js
@@ -3,7 +3,7 @@ const path = process.argv[2]
 
 params = params.map(value => Buffer.from(value, 'base64').toString('utf-8'))
 
-const output = (await import([path])).default(...params)
+const output = await (await import([path])).default(...params)
 
 if (output) {
     console.log(Buffer.from(output.toString(), 'utf-8').toString('base64'))
