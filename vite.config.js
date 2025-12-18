c7ffd5cf89c3df3224d0c27409d6fe8b8208de3b
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2025-12-18 14:27:40 +0100

    fix: eslint

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,36 @@
 import vituum from 'vituum'
 import { resolve } from 'path'
 import latte from './index.js'
+import process from 'node:process'
 
 export default {
-    plugins: [
-        vituum(),
-        latte({
-            filters: {
-                icon: 'latte/IconFilter.php',
-                hello: (value, there) => {
-                    return 'Hello ' + there + ' ' + value
-                }
-            },
-            globals: {
-                template: resolve(process.cwd(), 'playground/templates/Layout/Main.latte'),
-                srcPath: resolve(process.cwd(), 'playground')
-            },
-            data: './playground/data/**/*.json',
-            renderTransformedHtml: (filename) => !filename.endsWith('.latte')
-        })
-    ],
-    resolve: {
-        alias: {
-            '/src': resolve(process.cwd(), 'playground')
-        }
+  plugins: [
+    vituum(),
+    latte({
+      filters: {
+        icon: 'latte/IconFilter.php',
+        hello: (value, there) => {
+          return 'Hello ' + there + ' ' + value
+        },
+      },
+      globals: {
+        template: resolve(process.cwd(), 'playground/templates/Layout/Main.latte'),
+        srcPath: resolve(process.cwd(), 'playground'),
+      },
+      data: './playground/data/**/*.json',
+      renderTransformedHtml: filename => !filename.endsWith('.latte'),
+    }),
+  ],
+  resolve: {
+    alias: {
+      '/src': resolve(process.cwd(), 'playground'),
     },
-    publicDir: resolve(process.cwd(), 'public'),
-    build: {
-        manifest: true,
-        emptyOutDir: false,
-        modulePreload: false,
-        outDir: resolve(process.cwd(), 'public')
-    }
+  },
+  publicDir: resolve(process.cwd(), 'public'),
+  build: {
+    manifest: true,
+    emptyOutDir: false,
+    modulePreload: false,
+    outDir: resolve(process.cwd(), 'public'),
+  },
 }
